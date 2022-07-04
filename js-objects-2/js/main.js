4346ad7a3b6249174a72f39c10f949d0f4cd0aec
RC2PC2 <petriccarcc@gmail.com>
2022-07-04 10:55:42 +0200

    js-objects examples + vue-intro-scaffolding

---
+++ js-objects-2/js/main.js
@@ -0,0 +1,11 @@
+//  Operatore spread
+// const array = [0, 2, 4, 6, 8, 10];
+// const summedArray = [1,2,3,4,5];
+// const newArray = [...array, ...summedArray];
+// console.log(newArray);
+
+const [a, , b] = [10, 20, 30, 40, 50];
+
+console.log(a, b);
+// console.log(rest);
+// expected output: Array [30,40,50]
